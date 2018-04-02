ac9979bf809a6e8c9b98527a452484abcaab2233
Devan McCallum <demc9402@colorado.edu>
2018-04-02 14:45:00 -0600

    register php script

---
+++ register.php
@@ -0,0 +1,46 @@
+<?php
+/* Registration process, inserts user info into the database
+ */
+
+// Set session variables to be used on profile.php page
+$_SESSION['email'] = $_POST['email'];
+$_SESSION['first_name'] = $_POST['firstname'];
+$_SESSION['last_name'] = $_POST['lastname'];
+
+// Escape all $_POST variables to protect against SQL injections
+$first_name = $mysqli->escape_string($_POST['firstname']);
+$last_name = $mysqli->escape_string($_POST['lastname']);
+$email = $mysqli->escape_string($_POST['email']);
+$password = $mysqli->escape_string(password_hash($_POST['password'], PASSWORD_BCRYPT));
+$hash = $mysqli->escape_string( md5( rand(0,1000) ) );
+
+// Check if user with that email already exists
+$result = $mysqli->query("SELECT * FROM users WHERE email='$email'") or die($mysqli->error());
+
+// We know user email exists if the rows returned are more than 0
+if ( $result->num_rows > 0 ) {
+
+    $_SESSION['message'] = 'User with this email already exists!';
+    header("location: homepage.php");
+
+}
+else { // Email doesn't already exist in a database, proceed...
+
+    // active is 0 by DEFAULT (no need to include it here)
+    $sql = "INSERT INTO users (first_name, last_name, email, password, hash) "
+            . "VALUES ('$first_name','$last_name','$email','$password', '$hash')";
+
+    // Add user to the database
+    if ( $mysqli->query($sql) ){
+
+        $_SESSION['logged_in'] = true; // So we know the user has logged in
+        header("location: profile.php");
+
+    }
+
+    else {
+        $_SESSION['message'] = 'Registration failed!';
+        header("location: homepage.php");
+    }
+
+}
