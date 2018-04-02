5160c84ded9a23d7362078622bfd6b01ba96fe09
Devan McCallum <demc9402@colorado.edu>
2018-04-02 10:21:41 -0600

    Connect php files to database

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+/* Database connection settings */
+$host = 'localhost';
+$user = 'root';
+$pass = 'Qn9tvCerg4xxfqpn';
+$db = 'accountInfo';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
