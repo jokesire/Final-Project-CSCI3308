efba7ecea7e7850d4b8fa46c28c7b9c6408d4ac9
david-blair <36647620+david-blair@users.noreply.github.com>
2018-04-20 10:45:00 -0600

    Add files via upload

--- test.php
+++ test.php
@@ -16,6 +16,8 @@
 	$tvalue5 =  $_REQUEST['tlocationthree'];
 	$tvalue6 =  $_REQUEST['tdate'];
 	$tvalue7 =  $_REQUEST['tphone'];
+	$index = 0;
+	$score = 0;
 //Connecting to sql db.
 $mysqli = mysqli_connect('localhost','root','','test');
 //Sending form data to sql db.
@@ -23,13 +25,86 @@ echo "CONNECT";
 if(!$mysqli){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-$sql = "INSERT INTO SelfInformation (email , name , nickname , major , locationtype , locationdetail1 , locationdetail2  , daymet , phonenumber) VALUES ('1' , '2','$value1', '$value2', '$value3','$value4', '$value5', '$value6' , '$value7')";
-$sql = "INSERT INTO SearchingFor ( name , nickname , major , locationtype , locationdetail1 , locationdetail2  , daymet , phonenumber) VALUES (  '$tname','$tvalue1', '$tvalue2', '$tvalue3','$tvalue4', '$tvalue5', '$tvalue6' , '$tvalue7')";
+$id = $mysqli->query("SELECT id FROM SearchingFor ORDER BY id DESC LIMIT 1");
+
+while($row = $id->fetch_assoc() ){
+      $index = $row['id'] + 1 ;
+}
+
+ 
+$result = $mysqli->query("SELECT * FROM SelfInformation WHERE major='$tvalue2'");
+
+
+if ( $result->num_rows > 0 ){
+	
+	while($row = $id->fetch_assoc() ){
+		$score = 1;
+		if('$value1' == $row['nickname']){
+			$score = $score + 1;
+		}
+		if('$value3' == $row['locationtype']){
+			$score = $score + 1;
+		}
+		if('$value4' == $row['locationdetail1']){
+			$score = $score + 1;
+		}
+		if('$value4' == $row['locationdetail2']){
+			$score = $score + 1;
+		}
+		if('$value5' == $row['daymet']){
+			$score = $score + 1;
+		}
+		if('$value7' == $row['phonenumber']){
+			$score = $score + 1;
+		}
+		
+      $index = $row['id'] + 1 ;
+}
+	
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+$sql = "INSERT INTO SelfInformation (email , name , nickname , major , locationtype , locationdetail1 , locationdetail2  , daymet , phonenumber , id) VALUES ('1' , '2','$value1', '$value2', '$value3','$value4', '$value5', '$value6' , '$value7' , $index)";
 if(mysqli_query($mysqli, $sql)){
     echo "Records inserted successfully.";
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
 }
+
+$sql = "INSERT INTO SearchingFor ( name , nickname , major , locationtype , locationdetail1 , locationdetail2  , daymet , phonenumber , id ) VALUES (  '$tvalue0','$tvalue1', '$tvalue2', '$tvalue3','$tvalue4', '$tvalue5', '$tvalue6' , '$tvalue7' , $index)";
+if(mysqli_query($mysqli, $sql)){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
+}
+
+$result = $mysqli->query("SELECT * FROM SelfInformation WHERE major='$tvalue2'");
+
+
+if ( $result->num_rows > 0 ){
+	
+	print("HELLO");
+	
+}
+print("NO");
+
 
 mysqli_close($mysqli);
 ?>
