17e9c14d69d6a20aa892977ea56b3b4f184f0c9d
Devan McCallum <demc9402@colorado.edu>
2018-04-22 16:05:24 -0600

    added comment

--- db.php
+++ db.php
@@ -2,6 +2,6 @@
 /* Database connection settings */
 $host = 'localhost';
 $user = 'root';
-$pass = 'Qn9tvCerg4xxfqpn';
+$pass = 'Qn9tvCerg4xxfqpn';//Devan's Password
 $db = 'BoulderConnects';
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
