14146a9fe632962a480a8a3570fc9080d990f6b0
sequ5848 <sequ5848@colorado.edu>
2018-04-22 19:36:13 -0600

    changed db

--- db.php
+++ db.php
@@ -2,10 +2,5 @@
 /* Database connection settings */
 $host = 'localhost';
 $user = 'root';
-<<<<<<< HEAD
-$pass = 'aekdb';
-=======
-$pass = 'Qn9tvCerg4xxfqpn';//Devan's Password
->>>>>>> 17e9c14d69d6a20aa892977ea56b3b4f184f0c9d
-$db = 'BoulderConnects';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+$pass = '$SQ9184194606';
+
